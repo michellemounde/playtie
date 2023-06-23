33de78928db7347fb8fe7c9c613095c119439a56
Michelle Mounde <mitchmounde@gmail.com>
2023-06-23 15:53:43 +0300

    feat: scrape song titles from youtube playlist

--- backend/routes/api/links.js
+++ backend/routes/api/links.js
@@ -1,14 +1,35 @@
 const express = require('express');
 
+const { check } = require('express-validator');
+const { handleValidationErrors } = require('../../utils/validation');
+const { startBrowser, scraperController } = require('../../utils/scraper');
+
 const router = express.Router();
+
+const whitelist = ['www.youtube.com'];
+
+const validateUrl = [
+  check('url')
+    .notEmpty()
+    .isURL({ allow_underscores: true, host_whitelist: whitelist })
+    .withMessage('Please provide a Youtube link.'),
+  handleValidationErrors
+]
 
 // Submit playlist link
 // POST /
-router.post('/', (req, res, next) => {
-  const { url } = req.body;
+router.post(
+  '/',
+  validateUrl,
+  (req, res, next) => {
+    const { url } = req.body;
+    console.log(url);
 
-  console.log(url);
-  return res.json({ link: url})
-})
+    const browserInstance = startBrowser();
+    const songTitles = scraperController(browserInstance, url);
+
+    return res.json({ link: url })
+  }
+)
 
 module.exports = router;
--- backend/utils/scraper.js
+++ backend/utils/scraper.js
@@ -5,14 +5,14 @@ async function startBrowser() {
   let browser;
 
   try {
-    console.log("Opening the browser...");
+    console.log('Opening the browser...');
     browser = await puppeteer.launch({
       headless: false,
       args: ["--disable-setuid-sandbox"],
       'ignoreHTTPSErrors': true
     })
   } catch (err) {
-    console.warn("Could not create a browser instance => : ", err)
+    console.warn('Could not create a browser instance => : ', err)
   }
 
   return browser;
@@ -20,28 +20,37 @@ async function startBrowser() {
 
 // Page Scraper
 const pageScraper = {
-  url: 'http://books.toscrape.com',
+  //url: 'http://books.toscrape.com',
   async scraper(browser) {
     let page = await browser.newPage();
     console.log(`Navigating to ${this.url}...`);
     await page.goto(this.url)
+    // Wait for the required DOM to be rendered
+    await page.waitForSelector('ytd-section-list-renderer');
+    // Get the list of song titles
+    let titles = await page.$$eval('.ytd-playlist-video-list-renderer > ytd-playlist-video-renderer', songs => {
+      songs = songs.map(el => el.querySelector('h3 > a').title)
+      return songs;
+    })
+    console.log(titles);
   }
 }
 
-// Initialize browser instance
-let browserInstance = startBrowser();
-
 // Scraper Controller
-async function scraperController(browserInstance) {
+async function scraperController(browserInstance, url) {
   let browser;
 
   try {
     browser = await browserInstance;
+    pageScraper.url = url;
     await pageScraper.scraper(browser);
   } catch (err) {
-    console.warn("Could not resolve the browser instance => : ", err)
+    console.warn('Could not resolve the browser instance => : ', err)
   }
 }
 
 // Initialize scraping
-scraperController(browserInstance);
+module.exports = {
+  startBrowser,
+  scraperController
+}
