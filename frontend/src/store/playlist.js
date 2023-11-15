48826dc22c6462c3cabde3d264895474e99c7126
Michelle Mounde <mitchmounde@gmail.com>
2023-11-15 18:17:47 +0300

    feat: add pseudo-ish code for setting spotify playlist

--- frontend/src/store/playlist.js
+++ frontend/src/store/playlist.js
@@ -1,49 +1,63 @@
 import { csrfFetch } from './csrf';
 
 const GET_PLAYLIST = 'playlist/getPlaylist';
-const CREATE_PLAYLIST = 'playlist/createPlaylist'
+const SET_PLAYLIST = 'playlist/setPlaylist';
 
-const getPlaylist = playlist => {
-  return {
-    type: GET_PLAYLIST,
-    payload: playlist
-  }
-};
+const getPlaylist = (playlist) => ({
+  type: GET_PLAYLIST,
+  payload: playlist,
+});
 
+const setPlaylist = (playlist) => ({
+  type: SET_PLAYLIST,
+  payload: playlist,
+});
 
-export const getYoutubePlaylist = link => async dispatch => {
+export const getYoutubePlaylist = (link) => async (dispatch) => {
   const { url } = link;
 
   const res = await csrfFetch('/api/playlist', {
     method: 'POST',
     // headers will be set by csrfFetch
-    body: JSON.stringify({ url })
-    });
+    body: JSON.stringify({ url }),
+  });
 
   if (res.ok) {
     const data = await res.json();
     dispatch(getPlaylist(data.playlist));
     return res;
   }
-}
+};
 
+export const createSpotifyPlaylist = (songs) => async (dispatch) => {
+  // TODO Convert to Promise.all() because it's several songs
+  spotifySongs = songs.map(async (song) => {
+    await csrfFetch(`Spotify song search api here`);
+    // Get song and add it to playlist
+  });
+
+  if (res.ok) {
+    const data = await res.json();
+    dispatch(setPlaylist(data.playlist));
+    return res;
+  }
+};
 
 const initialState = {
   youtube: null,
-  spotify: null
+  spotify: null,
 };
 
-
 const playlistReducer = (state = initialState, action) => {
-  let nextState = Object.assign({}, state);
+  const nextState = { ...state };
 
-  switch(action.type) {
+  switch (action.type) {
     case GET_PLAYLIST:
       nextState.youtube = action.payload;
       return nextState;
     default:
       return state;
   }
-}
+};
 
 export default playlistReducer;
